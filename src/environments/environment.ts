319c1e5b92c2ebafc66a93a1ccc6e88cf54b7cec
Jan Greger Hemb <jan.greger@gmail.com>
2019-02-28 09:43:05 +0100

    Removed unneeded code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB65zCx4ynVCpWfLWzaXJqXtRU6E74jIHc",
-    authDomain: "firestore-workshop-61f48.firebaseapp.com",
-    databaseURL: "https://firestore-workshop-61f48.firebaseio.com",
-    projectId: "firestore-workshop-61f48",
-    storageBucket: "firestore-workshop-61f48.appspot.com",
-    messagingSenderId: "551859611911"
+    apiKey: 'AIzaSyB65zCx4ynVCpWfLWzaXJqXtRU6E74jIHc',
+    authDomain: 'firestore-workshop-61f48.firebaseapp.com',
+    databaseURL: 'https://firestore-workshop-61f48.firebaseio.com',
+    projectId: 'firestore-workshop-61f48',
+    storageBucket: 'firestore-workshop-61f48.appspot.com',
+    messagingSenderId: '551859611911'
   }
 };
 
